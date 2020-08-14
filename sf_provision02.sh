416b2c3088f8dea7e09f74772a500aee0cab7975
sopradev <45477573+sopradev@users.noreply.github.com>
2020-08-14 12:45:59 +0200

    script to provision with git, python, docker, terraform, ibmcloud pr…

--- sf_provision02.sh
+++ sf_provision02.sh
@@ -14,11 +14,12 @@ sudo wget https://releases.hashicorp.com/terraform/0.12.29/terraform_0.12.29_lin
 sudo unzip /tmp/terraform_0.12.29_linux_amd64.zip -d /usr/local/bin
 
 ## Installing the Terraform CLI and the IBM Cloud Provider plug-in
-mkdir $HOME/.terraform.d/plugins
+#mkdir $HOME/.terraform.d/plugins
+mkdir /root/.terraform.d/plugins
 wget https://github.com/IBM-Cloud/terraform-provider-ibm/releases/download/v1.10.0/terraform-provider-ibm_1.10.0_linux_amd64.zip -P /tmp
-unzip /tmp/terraform-provider-ibm_1.10.0_linux_amd64.zip -d $HOME/.terraform.d/plugins/
+#unzip /tmp/terraform-provider-ibm_1.10.0_linux_amd64.zip -d $HOME/.terraform.d/plugins/
+unzip /tmp/terraform-provider-ibm_1.10.0_linux_amd64.zip -d /root/.terraform.d/plugins/
 #mv $HOME/Downloads/terraform-provider-ibm* $HOME/.terraform.d/plugins/
-
 
 ## DOCKER
 ## https://docs.docker.com/engine/install/centos/
@@ -48,3 +49,50 @@ sudo yum install -y docker-ce-19.03.12-3.el7 docker-ce-cli-19.03.12-3.el7 contai
 sudo systemctl start docker
 sudo systemctl enable docker
 
+
+
+
+
+
+
+
+
+
+## Config sopra repo
+xrepo_temp=/tmp/sopra-sap-hana
+xrepo_gitlab=innersource.soprasteria.com/roumen.alvarado/ansible-test-00.git
+#xrepo_gitlab=innersource.soprasteria.com:luis-alberto.pedroza/IBM-SAP-POC.git
+xjenkins_home=/home/jenkins_home
+
+# remove all docker containers and images
+docker kill $(docker ps -q)
+#docker stop $(docker ps -a -q)
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -q)
+
+wait $!
+
+# remove innersource repo if exists
+sudo rm -rf $xrepo_temp
+
+# clone innersource repo
+#git clone ssh://git@innersource.soprasteria.com/roumen.alvarado/ansible-test-00.git $xrepo_temp
+############git clone ssh://git@$xrepo_gitlab $xrepo_temp
+#git clone git@$xrepo_gitlab $xrepo_temp
+
+wait $!
+
+# remove JENKINS_HOME
+sudo rm -rf $xjenkins_home
+############sudo mkdir $xjenkins_home
+############sudo chmod 777 $xjenkins_home
+############cp -R /tmp/sopra-sap-hana/jenkins/jenkins_home/credentials.xml $xjenkins_home/
+
+mkdir $xjenkins_home/secrets
+############cp -R /tmp/sopra-sap-hana/jenkins/jenkins_home/secret* $xjenkins_home/
+
+wait $!
+
+#
+#ansible-playbook /tmp/sopra-sap-hana/ansible/site.yml -vvv
+############ansible-playbook /tmp/sopra-sap-hana/ansible/site.yml
