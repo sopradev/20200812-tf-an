c9baecdcb76985a8c0e1f6fa0fc9fd69bf3c0196
sopradev <45477573+sopradev@users.noreply.github.com>
2020-08-14 08:03:47 +0200

    Add files via upload

--- sf_provision02.sh
+++ sf_provision02.sh
@@ -5,7 +5,7 @@
 sudo yum install -y epel-release git libc6-compat ansible python-devel py-pip python3 sshpass openssh-client
 
 
-# DOCKER
+## DOCKER
 # https://docs.docker.com/engine/install/centos/
 # Uninstall old versions
 
@@ -19,16 +19,16 @@ sudo yum install -y epel-release git libc6-compat ansible python-devel py-pip py
 #                    docker-engine
 				  
 				  
-sudo yum install -y yum-utils
+# sudo yum install -y yum-utils
 
-# set up the Docker repository
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
+## set up the Docker repository
+# sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
 
-# Install Docker Engine
-#sudo yum install docker-ce docker-ce-cli containerd.io
-#sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
-sudo yum install -y docker-ce-19.03.12-3.el7 docker-ce-cli-19.03.12-3.el7 containerd.io
+## Install Docker Engine
+###sudo yum install docker-ce docker-ce-cli containerd.io
+###sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+#sudo yum install -y docker-ce-19.03.12-3.el7 docker-ce-cli-19.03.12-3.el7 containerd.io
 
-# Start Docker
-sudo systemctl start docker
+## Start Docker
+#sudo systemctl start docker
 
