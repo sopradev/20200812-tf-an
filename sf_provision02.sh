421c873ee7a070cb5ca4708481eb6282656ae3c5
sopradev <45477573+sopradev@users.noreply.github.com>
2020-08-14 07:33:17 +0200

    Add files via upload

--- sf_provision02.sh
+++ sf_provision02.sh
@@ -4,4 +4,32 @@
 #sudo yum install -y ansible
 sudo yum install -y epel-release git libc6-compat ansible python-devel py-pip python3 sshpass openssh-client
 
-#sudo yum install -y git libc6-compat python-devel py-pip python3 sshpass openssh-client
+
+# DOCKER
+# https://docs.docker.com/engine/install/centos/
+# Uninstall old versions
+sudo yum remove -y docker \
+                   docker-client \
+                   docker-client-latest \
+                   docker-common \
+                   docker-latest \
+                   docker-latest-logrotate \
+                   docker-logrotate \
+                   docker-engine
+				  
+				  
+sudo yum install -y yum-utils
+
+# set up the Docker repository
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+# Install Docker Engine
+#sudo yum install docker-ce docker-ce-cli containerd.io
+#sudo yum install docker-ce-<VERSION_STRING> docker-ce-cli-<VERSION_STRING> containerd.io
+sudo yum install -y docker-ce-19.03.12-3.el7 docker-ce-cli-19.03.12-3.el7 containerd.io
+
+# Start Docker
+sudo systemctl start docker
+
