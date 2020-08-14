9d4d97ea594de496460fda28d60820de0e736555
sopradev <45477573+sopradev@users.noreply.github.com>
2020-08-14 06:29:14 +0200

    Add files via upload

--- sf_provision02.sh
+++ sf_provision02.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-sudo yum install -y epel-release
-sudo yum install -y ansible
+#sudo yum install -y epel-release
+#sudo yum install -y ansible
+sudo yum install -y epel-release git libc6-compat ansible python-devel py-pip python3 sshpass openssh-client
 
 #sudo yum install -y git libc6-compat python-devel py-pip python3 sshpass openssh-client
